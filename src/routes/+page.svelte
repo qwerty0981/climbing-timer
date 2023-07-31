09d4f406d01e761232b68d9d834a856ee8c5e20d
cerras0981 <contact@cerras.dev>
2023-07-31 00:18:24 -0400

    initial mui and big buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
 // @ts-nocheck
 
 	import { onMount } from 'svelte';
+	import Card, { Content } from '@smui/card'
+  import Button from "@smui/button";
 
 	class Timer {
 		_start = null;
@@ -38,12 +40,26 @@
 		}
 	}
 	
+  let state = "stop";
+
 	let timer = null;
 	let clock = null;
 	let now = new Date();
 	let timings = [];
 	let delta = 0;
 	let run = 1;
+
+  function stateChange(nextState) {
+    if (nextState === "stop") {
+      clearTimer()
+      if (state !== "resting") {
+        run++;
+      }
+    } else {
+      startTimer(nextState);
+    }
+    state = nextState;
+  }
 
 	function startTimer(type) {
 		if (timer !== null) {
@@ -83,82 +99,67 @@
 	$: {
 		if (timer !== null) {
 			delta = timer.elapsedTime(now);
+      if (delta < 0) {
+        delta = 0;
+      }
 		}
-	}
-
-	$: {
-		if (delta < 0) {
-			delta = 0;
-		}
-	}
-
-	$: {
-		console.log(timings);
-		console.log(run);
 	}
 </script>
 
-<div class="content">
-	<div class="column">
-		<h1>Time: {delta.toFixed(1)}</h1>
-		<button disabled={timer !== null && ["climbing", "lowering"].includes(timer.type)} on:click={() => startTimer("climbing")}>Climbing</button>
-		<button disabled={timer === null || ["take", "lowering", "resting"].includes(timer.type)} on:click={() => startTimer("take")}>Take</button>
-		<button disabled={timer === null || ["lowering", "resting"].includes(timer.type)} on:click={() => startTimer("lowering")}>Lowering</button>
-		<button disabled={timer === null || ["resting", "take", "climbing"].includes(timer.type)} on:click={() => startTimer("resting")}>Resting</button>
-		<button disabled={timer === null || ["climbing", "take"].includes(timer.type)} on:click={() => { clearTimer(); run++;} }>Stop</button>
+<Card padded variant="raised" style="height: 100%">
+  <div class="timer">
+    <h1>Time: {delta.toFixed(1)}</h1>
+    {#if state === "stop"}
+      <Button class="user-button" style="background-color: green" variant="raised" on:click={() => stateChange("climbing")}>Climbing</Button>
+    {:else if state === "climbing"}
+      <Button class="user-button" style="background-color: orange" variant="raised" on:click={() => stateChange("take")}>Take</Button>
+      <Button class="user-button" style="background-color: red" variant="raised" on:click={() => stateChange("lowering")}>Lowering</Button>
+    {:else if state === "take"}
+      <Button class="user-button" style="background-color: green" variant="raised" on:click={() => stateChange("climbing")}>Climbing</Button>
+      <Button class="user-button" style="background-color: red" variant="raised" on:click={() => stateChange("lowering")}>Lowering</Button>
+    {:else if state === "lowering"}
+      <Button class="user-button" style="background-color: blue" variant="raised" on:click={() => stateChange("resting")}>Resting</Button>
+      <Button class="user-button" style="background-color: red" variant="raised" on:click={() => stateChange("stop")}>Stop</Button>
+    {:else if state === "resting"}
+      <Button class="user-button" style="background-color: green" variant="raised" on:click={() => stateChange("climbing")}>Climbing</Button>
+      <Button class="user-button" style="background-color: red" variant="raised" on:click={() => stateChange("stop")}>Stop</Button>
+    {/if}
+    </div>
+    {#if state === "stop"}
+    <div style="flex-grow: 1">
+      <h1>Summary</h1>
+      <table style="width: 100%">
+        <tr>
+          <th>Run</th>
+          <th>Time on the wall</th>
+          <th>Climbing</th>
+          <th>Resting</th>
+          <th>Total Time (s)</th>
+        </tr>
 
-		{#if timings.length > 0}
-			<table>
-				<tr>
-					<th>Run #</th>
-					<th>Type</th>
-					<th>Time (s)</th>
-				</tr>
-			{#each timings as timer}
-				<tr>
-					<td>{timer.run}</td>
-					<td>{timer.type}</td>
-					<td>{timer.totalTime()}</td>
-				</tr>
-			{/each}
-			</table>
-		{/if}
-
-		{#if run > 1}
-			<h1>Summary</h1>
-			<table>
-				<tr>
-					<th>Run</th>
-					<th>Time on the wall</th>
-					<th>% Climbing</th>
-					<th>% Resting</th>
-					<th>Total run time</th>
-				</tr>
-
-				{#each new Set(timings.map(t => t.run)) as runNumber}
-					{@const wallTime = timings.
-							 filter(t => t.run === runNumber && ["climbing", "take"].includes(t.type)).
-							 reduce((old, next) => old + next.totalTime(), 0)}
-					<tr>
-						<td>{runNumber}</td>
-						<td>{wallTime.toFixed(1)}</td>
-						<td>{(timings.
-							filter(t => t.run === runNumber && t.type === "climbing").
-							reduce((old, next) => old + next.totalTime(), 0) / wallTime * 100).toFixed()}</td>
-						<td>{(timings.
-							filter(t => t.run === runNumber && t.type === "take").
-							reduce((old, next) => old + next.totalTime(), 0) / wallTime * 100).toFixed()}</td>
-						<td>{(timings.filter(r => r.run === runNumber).reduce((old, next) => old + next.totalTime(), 0)).toFixed(1)}s</td>
-					</tr>
-				{/each}
-			</table>
-		{/if}
-			
-	</div>
-</div>
+        {#each new Set(timings.map(t => t.run)) as runNumber}
+          {@const wallTime = timings.
+               filter(t => t.run === runNumber && ["climbing", "take"].includes(t.type)).
+               reduce((old, next) => old + next.totalTime(), 0)}
+          <tr>
+            <td>{runNumber}</td>
+            <td>{wallTime.toFixed(1)}s</td>
+            <td>{(timings.
+              filter(t => t.run === runNumber && t.type === "climbing").
+              reduce((old, next) => old + next.totalTime(), 0) / wallTime * 100).toFixed()}%</td>
+            <td>{(timings.
+              filter(t => t.run === runNumber && t.type === "take").
+              reduce((old, next) => old + next.totalTime(), 0) / wallTime * 100).toFixed()}%</td>
+            <td>{(timings.filter(r => r.run === runNumber).reduce((old, next) => old + next.totalTime(), 0)).toFixed(1)}s</td>
+          </tr>
+        {/each}
+      </table>
+    </div>
+    {/if}
+</Card>
 
 <style>
-	.content {
+	/* .content {
 		display: flex;
 		justify-content: center;
 		height: 100%;
@@ -166,15 +167,25 @@
 		background-color: white;
 		color: black;
 		overflow-y: scroll
-	}
-	
-	.column {
-		display: flex;
-		flex-direction: column;
-		gap: 0.5em;
-	}
+	} */
+  
+  .timer {
+    width: 100%;
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 0.5em;
+  }
+
+  * :global(.user-button) {
+    width: 100%;
+    flex-grow: 1;
+  }
 
 	table, th, td {
 		border: 1px solid;
+    text-align: center;
 	}
+
 </style>
